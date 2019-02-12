b7dec9058b1641c27c1fbe4953fe1e588c06c5bd
Mun <oneofthemoons@mun.local>
2019-02-12 22:58:30 +0300

    deb1

--- fillit/main.c
+++ fillit/main.c
@@ -108,7 +108,12 @@ int     main(int argc, char **argv)
                         break;
                     }
                 if (!figIn)
+                {
+                    system("clear");
+                    printf("fig: %d\n", curFig);
+                    printBitMap(bitMap);
                     break;
+                }
                 i = -1;
                 while (++i < 4)
                     bitMap.st[arrFig[curFig].pos.y + i] |= (arrFig[curFig].st[i] << arrFig[curFig].pos.x);
